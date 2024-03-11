71a48ca2b3cabc4257f184e60ae3a45fee63994d
Brahim Kerdad <kerdadbrah@cy-tech.fr>
2024-03-11 15:23:17 +0100

    Update ksvd.py to answer question 2

--- ksvd.py
+++ ksvd.py
@@ -1,21 +1,25 @@
-
+import pandas as pd
 import numpy as np
 from numpy.linalg import norm
-
 from omp import OMP
+ 
+eps=0.0001
+iterMax= 100
+ 
+ 
 
-def ksvd(X, D0, m, eps=10**-4, iterMax=100):
+def ksvd(X, D0, m):
     n, l = np.shape(X)
     _, k = np.shape(D0)
     A = np.zeros((k, l))  # matrice Lambda des représentations parcimonieuses
     D = D0  # initialisation du dictionnaire
-
+ 
     for _ in range(m):  # boucle principale, m=nombre d'itérations de KSVD
         # Représentation parcimonieuse des l signaux
         for p in range(l):
             alpha, R, it, index = OMP(X[:, p], D, eps, iterMax)
             A[:, p] = alpha.flatten()
-
+ 
         # Actualisation des atomes du dictionnaire, un à un (on a k atomes)
         for i in range(k):
             Ei = X - np.dot(D, A) + np.dot(D[:, i].reshape(-1, 1), A[i, :].reshape(1, -1))
@@ -23,6 +27,7 @@ def ksvd(X, D0, m, eps=10**-4, iterMax=100):
             if np.all(A[i, :] == 0):
                 v = np.random.rand(n)
                 D[:, i] = v / norm(v)
+ 
             else:
                 # Indices des composantes non nulles de A[i, :]
                 Wi = np.nonzero(A[i, :])[0]
@@ -38,5 +43,24 @@ def ksvd(X, D0, m, eps=10**-4, iterMax=100):
                     # Actualisation de l'atome n°i
                     D[:, i] = U[:, 0]
                     A[i, Wi] = S[0] * V[0, :]
-
+ 
     return D, A
+ 
+ 
+ 
+data1 = pd.read_excel("DonneesProjet.xlsx", header=None)
+data = data1.apply(pd.to_numeric, errors='coerce')# transformation object ---> float
+X = data.iloc[:, :100].values  # Sélectionner les 100 premières colonnes
+ 
+# Initialiser le dictionnaire D0
+n, l = X.shape
+k = 100  # Nombre d'atomes dans le dictionnaire
+D0 = np.random.rand(n, k)
+D0 = D0 / norm(D0, axis=0)  # Normaliser les colonnes
+ 
+# Appliquer l'algorithme k-SVD
+m = 10  # Nombre d'itérations de l'algorithme k-SVD
+D, A = ksvd(X, D0, m)
+ 
+# Enregistrer le dictionnaire dans un fichier CSV
+pd.DataFrame(D).to_csv("Dico-appris.csv", index=False)
