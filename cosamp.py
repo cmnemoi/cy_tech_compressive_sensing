3fd8f2a694b7fc77383cf26c95494f53943b2887
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-03-17 18:51:55 +0100

    Fix cosamp which was broken

--- cosamp.py
+++ cosamp.py
@@ -6,35 +6,27 @@ def Cosamp(x,D,s=None,eps=10**-4,iterMax=100):
     if s is None:
         s = int( ( len(D) / ( 2 * np.log(len(x)) ) ) )
 
-    n,k=np.shape(D)
-    it=0
-    alpha=np.zeros(k)
-    R=x
-    index=[]
-    A=np.empty((n,0))
-    ps=np.zeros(k)
-    while norm(R)>eps and it<iterMax:
-        for j in range (k):
-            ps[j]=np.abs(np.dot(D[:,j].T,R))/norm(D[:,j])
-        #sélection des atomes dont la contrib est parmi les 2s plus grandes
-        m=np.argpartition(ps,-2*s)[-2*s:]
-        #ajout des nouveaux indices aux anciens
-        V=set(index)|set(m)
-        index=list(V)
-        #index.append(m)
-        #Application des moindres carré
-        A=D[:,index]
-        alpha[index]=np.dot(np.linalg.pinv(A),x)
-        index=np.argpartition(np.abs(alpha),-s)[-s:]
-        #on recommence les moindres carrés avec les s atomes retenus
-        alpha=np.zeros(k)
-        A=D[:,index]
-        alpha[index]=np.dot(np.linalg.pinv(A),x)
-        #actualisation du résidu
-        R=x-np.dot(A,alpha[index])
-        it=it+1
-    return alpha, R, it, index
+    # Initialisation
+    k = D.shape[1]
+    alpha = np.zeros(k)
+    residual = x
+    it = 0
+    index = []
+    while it < iterMax and norm(residual) > eps:
+        # Calcul de la contribution de chaque atome au résidu
+        contributions = np.abs(D.T @ residual) / np.linalg.norm(D, ord=2, axis=0)
+        # Récupération des indices des 2s plus grandes contributions
+        indexes = np.argsort(contributions)[-2*s:]
+        index.extend(indexes)
+        index = list(set(index))
+        # Mise à jour de alpha par moindres carrés
+        A = D[:, index]
+        alpha[index] = np.linalg.pinv(A) @ x
+        # Mise à jour de r
+        residual = x - A @ alpha[index]
+        it += 1
 
+    return alpha, residual, it, index
 
 if __name__ == '__main__':
     D = np.array([
@@ -44,4 +36,4 @@ if __name__ == '__main__':
     ])
     x = np.array([4/3 - np.sqrt(2)/2, 4/3 + np.sqrt(2)/2, 2/3]).T
 
-    print(Cosamp(x, D, len(x)-1))
+    print(Cosamp(x, D))
