da685fb2d7e01f1519a9ea8464d5c956e6391168
Brahim <kerdadbrah@cy-tech.fr>
2024-03-17 16:23:04 +0100

    Fix irls.py

--- irls.py
+++ irls.py
@@ -1,30 +1,30 @@
 import numpy as np
-from numpy.linalg import norm
+from numpy.linalg import norm, inv
 
-def irls(x,D,p=2,eps=10**-4,iterMax=100):
-    n,k=np.shape(D)
-    alpha=np.zeros(k)
-    Q=np.zeros((k,k))
-    it=0
-    #initialisation de alpha
-    alpha0= D.T @ np.linalg.inv(D @ D.T) @ x
-    test=True
-    #Boucle principale
-    while test and it<iterMax:
-        #construction de la matrice Q
+def irls(x, D, iterMax=100, eps=10**-4, p=3):
+    n, k = np.shape(D)
+    alpha = np.zeros(k)
+    Q = np.zeros((k, k))
+    it = 0
+    # Initialisation de alpha
+    alpha0 = D.T @ inv(D @ D.T) @ x
+    test = True
+    # Boucle principale
+    while test and it < iterMax:
+        # Construction de la matrice Q
         for i in range(k):
-            z=(np.abs(alpha0[i])**2+eps)**(p/2-1)
-            Q[i,i]=1/z
-            #calcul du nouvel alpha
-            alpha=Q @ D.T @ np.linalg.inv(D @ D @ D.T) @ x
-            #critère d'arrêt:
-            if norm(alpha-alpha0)<np.sqrt(eps)/100 and eps<10**(-8):
-                test= False
-            else:
-                eps=eps/10
-                alpha0=alpha
-                it=+1
-
+            z = (np.abs(alpha0[i]) ** 2 + eps) ** (p / 2 - 1)
+            Q[i, i] = 1 / z
+        # Calcul du nouvel alpha
+        alpha = Q @ D.T @ inv(D @ Q @ D.T) @ x
+        # Critère d'arrêt
+        if norm(alpha - alpha0) < np.sqrt(eps) / 100 and eps < 10 ** (-8):
+            test = False
+        else:
+            eps = eps / 10
+            alpha0 = alpha
+            it += 1
+ 
     return alpha, it
 
 if __name__ == '__main__':
